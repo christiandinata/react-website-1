09b455e2964d87d1848b48cd0ab9eac679335558
Christiand6 <34437533+Christiand6@users.noreply.github.com>
2021-06-02 12:23:50 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Navbar from './components/Navbar.js';
-import Home from './components/pages/Home';
-import Products from './components/pages/Products';
-import Services from './components/pages/Services';
-import SignUp from './components/pages/SignUp';
-import Footer from './components/Footer';
-import ScrollTop from './components/ScrollTop';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
+import Footer from "./components/Footer";
+import ScrollTop from "./components/ScrollTop";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/services" component={Services}/>
-          <Route path="/products" component={Products}/>
-          <Route path="/sign-up" component={SignUp}/>
-        </Switch>
-        <Footer />
-        <ScrollTop />
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/services" component={Services} />
+					<Route path="/products" component={Products} />
+					<Route path="/sign-up" component={SignUp} />
+				</Switch>
+				<ScrollTop />
+			</Router>
+		</>
+	);
 }
 
 export default App;
